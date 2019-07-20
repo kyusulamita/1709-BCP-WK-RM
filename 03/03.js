79a61d25e2873e94b07654c166f11b04b4e7b782
kyusulamita <sulamitamorales@yahoo.com>
2019-07-20 15:47:57 -0400

    inital commit

---
+++ 03/03.js
@@ -0,0 +1,9 @@
+console.log(true == "1")
+const leftSide = Number(true);
+const rightSide = Number("1");
+
+// true => 1
+// false => 0
+
+console.log(leftSide, rightSide);
+console.log(leftSide === rightSide);
