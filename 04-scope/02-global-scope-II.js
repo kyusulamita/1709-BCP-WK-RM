909b1d8a18ada9d1335fa2a12d04591cc1e59b0c
kyusulamita <sulamitamorales@yahoo.com>
2019-07-21 10:38:51 -0400

    updated

--- 04-scope/02-global-scope-II.js
+++ 04-scope/02-global-scope-II.js
@@ -1,3 +1,4 @@
+//http://www.pythontutor.com/visualize.html#code=console.log%28waitForIt%29%3B%0Alet%20waitForIt%20%3D%20'here%20I%20am'%3B%0Aconsole.log%28waitForIt%29%3B&cumulative=false&heapPrimitives=nevernest&mode=edit&origin=opt-frontend.js&py=js&rawInputLstJSON=%5B%5D&textReferences=false
 /* EXAMPLE START ===========
 // reference error, trying to use it before it's defined! [let keyword]
 console.log(waitForIt);
@@ -16,7 +17,7 @@ console.log(waitForIt);
 ======== EXAMPLE END */
 
 
-/* EXAMPLE START ===========
+// /* EXAMPLE START ===========
 // with function you get behavior like this, functions declared with the `function` keyword are hoisted up
 
 funFunction();
@@ -26,4 +27,4 @@ function funFunction(){
 }
 
 funFunction();
-======== EXAMPLE END */
+// ======== EXAMPLE END */
--- 04-scope/06-nested-functions.js
+++ 04-scope/06-nested-functions.js
@@ -1,16 +1,19 @@
 /* consider nested functions */
+//http://www.pythontutor.com/visualize.html#code=let%20globalVar%20%3D%20'global'%3B%0Afunction%20outer%28%29%20%7B%0A%20%20let%20outerVar%20%3D%20'outer'%3B%0A%0A%20%20function%20inner%28%29%20%7B%0A%20%20%20%20let%20innerVar%20%3D%20'inner'%3B%0A%20%20%20%20console.log%28globalVar,%20outerVar,%20innerVar%29%3B%0A%20%20%7D%0A%0A%20%20inner%28%29%3B%0A%7D%0Aouter%28%29%3B&cumulative=false&heapPrimitives=nevernest&mode=edit&origin=opt-frontend.js&py=js&rawInputLstJSON=%5B%5D&textReferences=false
+// let globalVar = 'global'; // will work, before function call
+function outer() {
+  let outerVar = 'outer';
 
+  function inner() {
+    let innerVar = 'inner';
+    console.log(globalVar, outerVar, innerVar);
+  }
 
-// let globalVar = 'global';
-// function outer() {
-//   let outerVar = 'outer';
-//   function inner() {
-//     let innerVar = 'inner';
-//     console.log(globalVar, outerVar, innerVar);
-//   }
-//   inner();
-// }
-// outer();
+  inner();
+}
+// let globalVar = 'global'; // will work, before function call
+outer();
+// let globalVar = 'global'; // Won't work
 
 /* Example 2
 let collision = 'global';
@@ -37,14 +40,14 @@ outer('outer');
 
 // /* Example 4
 
-let collision = 'global';
-function outer(){
-  function inner() {
-    console.log(collision)
-  }
-  inner()
-}
-outer('outer');
+// let collision = 'global';
+// function outer(){
+//   function inner() {
+//     console.log(collision)
+//   }
+//   inner()
+// }
+// outer('outer');
 
 // */
 
