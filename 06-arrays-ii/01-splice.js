2068696882b560c3aab1de90e7f0fa3137583ced
Sulamita Morales <sulamitamorales@yahoo.com>
2019-07-22 17:36:23 -0400

    updated notes

--- 06-arrays-ii/01-splice.js
+++ 06-arrays-ii/01-splice.js
@@ -1,63 +1,38 @@
 /*
-.splice mutates the original array; 
+  .splice mutates the original array; 
 
-it's used to add or remove elements from the middle of an array (instead of adding or removing from either end  lets us know if the element is in the array
+  it's used to add or remove elements from the middle of an array (instead of adding or removing from either end  lets us know if the element is in the array
 
-@params startIdx {number} [optional]
-@params deleteCount {number} [optional]
-@params elementsToInsert [optional]
+  @params startIdx {number} [optional]
+  @params deleteCount {number} [optional]
+  @params elementsToInsert [optional]
 
-@returns elements that were spliced out of the array
+  @returns elements that were spliced out of the array
 */
 
-// let names = ['Paul', 'George', 'John', 'Ringo'];
-// let acrossTheUniverse = names.splice(1); // ['George', 'John', 'Ringo']
+// let names = ["Mr Bear", "Pusheen", "Planty", "Mr Sheep"];
+// let acrossTheUniverse = names.splice(1); // ["Pusheen", "Planty", "Mr Sheep"]
 
-// let acrossTheUniverse = names.splice(1, 2); // ['George', 'John']
+// let acrossTheUniverse = names.splice(1, 2); // ["Pusheen", "Planty"]
 
 
-// let acrossTheUniverse = names.splice(1, 2, 'Pusheen'); // ['George', 'John']
+// let acrossTheUniverse = names.splice(1, 2, "Theodore"); // ["Pusheen", "Planty"]
 // console.log(acrossTheUniverse);
 // console.log(names);
 
-// let acrossTheUniverse = names.splice(1, 2, 'Pusheen'); // ['George', 'John']
 
-// console.log(names);// ['Paul', 'Pusheen','Ringo']
+
+// let names = ["Mr Bear", "Pusheen", "Planty", "Mr Sheep"];
+// let acrossTheUniverse = names.splice(1, 0, "Theodore"); // []
+
+// console.log(names);
 // console.log(acrossTheUniverse);
 
 
-// let names = ['Paul', 'George', 'John', 'Ringo'];
-// let acrossTheUniverse = names.splice(1, 0, 'Pusheen'); // []
+// let names = ["Mr Bear", "Pusheen", "Planty", "Mr Sheep"];
+// let acrossTheUniverse = names.splice(); 
 
-// console.log(names);// ['Paul', 'Pusheen', 'George', 'John','Ringo']
-// console.log(acrossTheUniverse);
-
-
-// let names = ['Paul', 'George', 'John', 'Ringo'];
-// let acrossTheUniverse = names.splice(); // ['Paul', 'George', 'John', 'Ringo']
-
-// console.log("names", names); // ['Paul', 'George', 'John', 'Ringo']
+// console.log("names", names); // ["Mr Bear", "Pusheen", "Planty", "Mr Sheep"];
 // console.log(acrossTheUniverse); // []
 
-// names.splice(0);
 
-// names = [];
-
-// while(names.length > 0 ){
-//   names.pop();
-// }
-
-// console.log(names)
-
-
-// const names = ['Paul', 'George', 'John', 'Ringo'];
-// const acrossTheUniverse = names.splice(1); // [ 'George', 'John', 'Ringo']
-
-// console.log(names); // ['Paul']
-// console.log(acrossTheUniverse);
-
-
-// const pets = ['Pusheen', 'Mr. Bear'];
-// pets.splice(1);
-
-// console.log(pets);
