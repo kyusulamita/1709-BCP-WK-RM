e033584dd2e03e5b7c1a5c3663e296d24175955c
kyusulamita <sulamitamorales@yahoo.com>
2019-07-28 13:37:08 -0400

    recursion ii notes

---
+++ 13-recursion-ii/07.js
@@ -0,0 +1,33 @@
+/* 
+
+https://goo.gl/mVK9Jt
+
+https://goo.gl/MWfh9V
+*/
+
+function sumVals(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    let value = obj[key];
+    if (typeof value === 'object') {
+      sum += sumVals(value)
+    } else {
+      sum += value;
+    }
+  }
+  return sum;
+}
+
+let result = sumVals({
+  a: 1,
+  b: {
+    c: {
+      d: {
+        e: 2,
+        f: 3
+      }
+    }
+  }
+})
+
+console.log(result);
